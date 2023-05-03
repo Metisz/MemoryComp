1d9ce970d45aaa598d9237472ab5357f9bb1d1ff
Metisz <matis.marton5@gmail.com>
2023-05-03 20:30:42 +0200

    augh

--- Reakcio.xaml.cs
+++ Reakcio.xaml.cs
@@ -1,25 +1,12 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 
 namespace MemoryComp
 {
-	/// <summary>
-	/// Interaction logic for Reakcio.xaml
-	/// </summary>
 	public partial class Reakcio : Window
 	{
 		int jatekid = 2;
